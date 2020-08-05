a06338ea542a5c38ccf2b779bf7cf71f15554420
Pavel Tyukin <paveltyukinofficial@gmail.com>
2020-08-05 21:35:53 +0300

    hw 01-intro 01-sum

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,21 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+  constructor(options = {limit: 8, encoding: 'utf-8'}) {
     super(options);
+
+    this.limit = options.limit;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.size += chunk.length;
 
+    if (this.size > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
