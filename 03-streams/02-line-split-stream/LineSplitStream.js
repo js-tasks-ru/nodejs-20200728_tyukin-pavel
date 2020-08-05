7728b11fd02d62339135f91a5e6a8fddf21a64d0
Pavel Tyukin <paveltyukinofficial@gmail.com>
2020-08-05 22:50:33 +0300

    hw 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,24 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.chunkPart = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const tempSum = this.chunkPart + chunk;
+    const detach = tempSum.split(os.EOL);
+    this.chunkPart = detach.pop();
+
+    detach.forEach((chunk) => {
+      this.push(chunk);
+    });
+
+    callback(null);
   }
 
   _flush(callback) {
+    callback(null, this.chunkPart);
   }
 }
 
