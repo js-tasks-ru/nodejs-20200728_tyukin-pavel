5c7b81241d89d1c8a039a5d06862b46875b3bfbb
Pavel Tyukin <paveltyukinofficial@gmail.com>
2020-08-15 18:26:06 +0300

    fix 5-module 1-task

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,67 +1,31 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-const fs = require('fs').promises;
-const SpecificPathError = require('./SpecificPathError');
+const fs = require('fs');
 
 const server = new http.Server();
 
-async function readFiles(req, res, directory, pathname) {
-  const fileName = pathname.replace(/files\//ig, '');
-  if (fileName.indexOf('/') >= 0) {
-    res.statusCode = 400;
-    throw new SpecificPathError();
-  }
-
-  const entities = await fs.readdir(directory);
-
-  for (const entity of entities) {
-    const stat = await fs.stat(path.join(directory, entity));
-    if (!stat.isFile()) continue;
-    const content = await fs.readFile(path.join(directory, entity));
-    if (fileName === entity) {
-      return {file: entity, content};
-    }
-  }
-
-  res.statusCode = 404;
-  res.write('Такого файла нет', 'utf8');
-}
-
-
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-      readFiles(req, res, path.join(__dirname, 'files'), pathname)
-          .catch((error) => {
-            console.log(error);
-            switch (error.code) {
-              case 'BAD_PATH':
-                res.statusCode = 400;
-                res.end('400');
-                break;
-              case 'ENOENT':
-                res.statusCode = 404;
-                res.end('404');
-                break;
-              default:
-                res.statusCode = 500;
-                res.end('500');
-                break;
-            }
-          })
-          .then((file) => {
-            if (typeof file === 'undefined' || !file) {
-              res.statusCode = 500;
-              res.end('500');
-              return;
-            }
-
-            res.write(file.content);
-            res.end();
-          });
+      fs.createReadStream(filepath).on('error', (err) => {
+        console.error(err);
+        if (err.code === 'ENOENT') {
+          if (pathname.indexOf('/') > 0) {
+            res.writeHead(400);
+            res.end('Bad request');
+          } else {
+            res.writeHead(404);
+            res.end('Not found');
+          }
+        } else {
+          res.statusCode = 500;
+          res.end('Internal server error');
+        }
+      }).pipe(res);
       break;
     default:
       res.statusCode = 501;
